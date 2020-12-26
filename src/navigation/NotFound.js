0b819e4b4369e66a4323cbf67845578ea0d19102
IRONMTN\SShah.prt <saurabh.shah.prtnr@ironmountain.com>
2020-12-26 17:00:51 +0530

    Router+MUI+MUICustomTheme

---
+++ src/navigation/NotFound.js
@@ -0,0 +1,16 @@
+import React from "react";
+// import { Typography, Button } from "@material-ui/core";
+// import { Link } from "react-router-dom";
+
+export const NotFound = () => {
+  return (
+    <div>
+      {/* <Typography variant="h2">404: page not found!</Typography>
+
+      <Button variant="contained" color="primary" href="/">
+        Go Home
+      </Button> */}
+      Page Not Found!
+    </div>
+  );
+};
